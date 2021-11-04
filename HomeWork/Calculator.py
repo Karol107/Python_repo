f398b6bfcf62453907902d43a3d2a73c63357090
Karol107 <pMGKiWqZ4JZMxh@h>
2021-11-04 21:56:11 +0100

    PracaDomowa_final_calculator

--- HomeWork/Calculator.py
+++ HomeWork/Calculator.py
@@ -16,16 +16,38 @@ print("1.Dodawanie")
 print("2.Odejmowanie")
 print("3.Mnożenie")
 print("4.Dzielenie")
-
+print("0.Zakoncz")
 while True:
-    choice = input("Wybierz operację(1/2/3/4): ")
+    choice = input("Wybierz operację(1/2/3/4/0): ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '0'):
+        if choice == '0':
+            print('Zakoncz')
+            break
         num1 = float(input("Podaj pierwszą liczbę: "))
         num2 = float(input("Podaj drugą liczbę: "))
 
-        for choice == 0:
-            print('Zakoncz',return 0)
+        if choice == '1':
+            dodawanie = num1 + num2
+            print('wynik dodawaniato:\n', dodawanie)
+
+        if choice == '2':
+            odejmowanie = num1 - num2
+            print('wynik odejmowania:\n', odejmowanie)
+
+        if choice == '3':
+            mnnozenie = num1 * num2
+            print('wynik mnozenia:\n', mnnozenie)
+
+        if choice == '4':
+            if num2 == 0:
+              print('Pamietaj cholero nie dziel przez "0"!')
+
+            else:
+                dzielenie = num1 / num2
+                print('wynik dzielenia:\n', dzielenie)
+
+
 
     else:
         print("Błędna wartość, podaj poprawną")
