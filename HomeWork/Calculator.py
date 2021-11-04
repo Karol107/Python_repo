77e1cc925bf9a07e5cff7c32705ec86ea2d8d446
Karol107 <pMGKiWqZ4JZMxh@h>
2021-11-04 21:32:41 +0100

    pracadomowa

---
+++ HomeWork/Calculator.py
@@ -0,0 +1,31 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+
+print("Wybierz operację.")
+print("1.Dodawanie")
+print("2.Odejmowanie")
+print("3.Mnożenie")
+print("4.Dzielenie")
+
+while True:
+    choice = input("Wybierz operację(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Podaj pierwszą liczbę: "))
+        num2 = float(input("Podaj drugą liczbę: "))
+
+        for choice == 0:
+            print('Zakoncz',return 0)
+
+    else:
+        print("Błędna wartość, podaj poprawną")
