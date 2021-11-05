3e5a711524857b872bc648cb9bd73a40cd83bc9b
Karol107 <pMGKiWqZ4JZMxh@h>
2021-11-05 21:03:18 +0100

    zdanie domowe kurs euro ver 1.0

--- HomeWork/Kursy_walut.py
+++ HomeWork/Kursy_walut.py
@@ -50,8 +50,7 @@ print(response.text)
 print("-" * 50, '\nKurs Euro:                ', 'EURO','\nData i godzina:            ' 'Date_and_Time',
       '\nCzas wykonania zapytania: ', 'Data i godzina' ) #gdy sie powiedzie
 print("-" * 50, '\nBlad pozyskania danych    "Request Timeout"','\nData i godzina:            ' 'Date_and_Time',
-      '\nCzas wykonania zapytania: ', 'Data i godzina' ) #gdy Timeout
-
+      '\nCzas wykonania zapytania: ', 'Data i godzina' ) #"gdy Timeou"
 
 
 
