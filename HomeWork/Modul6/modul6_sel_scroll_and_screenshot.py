8c1ce2e5508c671035949725ed182658c86f7ef9
kuriata <karol.kuriata@nokia.com>
2021-12-18 14:36:27 +0100

    zadanie domowe scroll

--- HomeWork/Modul6/modul6_sel_scroll_and_screenshot.py
+++ HomeWork/Modul6/modul6_sel_scroll_and_screenshot.py
@@ -1,24 +1,16 @@
 from selenium import webdriver
 import helpers.selenium_fun_wspomagajace_Karol as helper
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 URL = "https://fabrykatestow.pl"
-
-
 driver = webdriver.Chrome('C:\Drivers\Chromedriver.exe')
 driver.get(URL)
 driver.maximize_window()
 driver.find_element_by_id('menu-item-1871').click()
-time.sleep(5)
+time.sleep(2)
 helper.wait_for_visibility_of_element(driver, '//*[@id="content"]/div/div/div/section[5]/div/div/div/div[2]/div/section[1]/div/div/div[1]/div/div/div/div/div/a').click()
-
-scrolling_element = driver.find_element_by_xpath('//*[@id="content"]/div/div/div/div/div/div/div/section[6]/div[2]/div/div/div/div/section/div/div/div[1]/div/div/div[1]/div/div/img')
-time.sleep(5)
-scrolling_element.location_once_scrolled_into_view
-# driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scrolling_element)
-
-# driver.execute_script('window.scrollBy(0, document.body.scrollHeight)') #skroll na koniec srtrony
+Pokemon_capture = driver.find_element_by_xpath('//*[@id="content"]/div/div/div/div/div/div/div/section[6]/div[2]/div/div/div/div/section/div/div/div[1]/div/div/div[1]/div/div/img')
+Pokemon_capture.location_once_scrolled_into_view
 driver.save_screenshot("./Zwierzu.png")
 driver.quit()
 
